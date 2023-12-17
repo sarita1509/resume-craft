a9fce5879d5cedadc6be35fbb55f7da1812e227f
sarita1509 <krsarita5@gmail.com>
2023-12-17 22:43:29 +0530

    "commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+import 'react-router-dom';
+import React from 'react';
+import {  Route,Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Aboutus from './Pages/About Us/Aboutus';
+import PersonalInformation from './Components/PersonalInformation';
+import Education from './Components/Education';
+import WorkExperience from './Components/WorkExperience';
+import Myresume from './Components/Myresume';
+import KeySkills from './Components/Keyskills';
+import Templetes from './Pages/Home/Templetes';
+import Preview from './Pages/Preview/Preview';
+import GettingStarted from './Components/GettingStarted';
+import Detailfilling from './Pages/Details Filling/Detailfilling';
+
+
+  function App() {
+
+  return (<>
+
+       {/* all thr routes are defined with perticular path for Navigation Between Pages */}
+       
+ <Routes> <Route path="/Detailfilling" element={<Detailfilling/>} />
+          <Route exact path='/Templete' element={<Templetes/>}/>
+          <Route exact path='/header' element={<Header/>}/>
+          <Route exact path="/personal" element={<PersonalInformation/>}/>
+          <Route exact path="/education" element={<Education/>}/>
+          <Route exact path="/about-us" element={<Aboutus/>}/>
+          <Route exact path="/" element={<GettingStarted/>}/>
+          <Route exact path='/work' element={<WorkExperience/>}/>
+          <Route exact path='/myresume' element={<Myresume/>}/>
+          <Route exact path='/skills' element={<KeySkills/>}/>
+      <Route exact path='/preview' element={ <Preview /> } />
+      
+  </Routes>
+  
+   </>
   );
 }
-
 export default App;
+
+
+
+
+
---
+++ src/Data/data.js
@@ -0,0 +1,24 @@
+import Resume2 from '../Templetes/Resume2'
+import Resume3 from '../Templetes/Resume3'
+import Resume4 from '../Templetes/Resume4'
+
+// Stores All Templates As a json State 
+const temp = [
+    {
+        thumbnail: '/images/Templete2.jpg',
+        data: <Resume2 />
+    },
+
+    {
+        thumbnail: '/images/Templete3.jpg',
+        data: <Resume3 />
+    },
+
+    {
+        thumbnail: '/images/Templete4.jpg',
+        data: <Resume4 />
+    }
+
+]
+
+export default temp;
